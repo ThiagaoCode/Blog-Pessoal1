a609aa532315bb0cf65b808e5916b897f1970ae8
unknown <thi.pedroso.soares@gmail.com>
2021-07-26 13:05:31 -0300

    Iniciando o blog pessoal 1

---
+++ primeira insercao no banco de dados com conexao web.sql
@@ -0,0 +1,4 @@
+SELECT * FROM blog_pessoal.postagens;
+
+INSERT INTO blog_pessoal.postagens (date, texto, titulo)
+	VALUES ('2021-07-26 10:10:10.000', 'chamando na primeira conexao web com banco de dados', 'API Rest Spring nessa boba!');
